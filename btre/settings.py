2bb3eaa67d9eb42961d3ade5b11bd588dd6a79e0
Chetri <karan.chetri@capgemini.com>
2022-12-01 12:54:31 +0530

    Final commit

--- btre/settings.py
+++ btre/settings.py
@@ -31,9 +31,11 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'pages.apps.PagesConfig',   #added for pages app
+    'pages.apps.PagesConfig',        #added for pages app
     'listings.apps.ListingsConfig',  #added for listings app
     'realtors.apps.RealtorsConfig',  #added for realtors app
+    'accounts.apps.AccountsConfig',  #added for accounts app
+    'contacts.apps.ContactsConfig',  #added for contacts app
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -131,6 +133,19 @@ STATICFILES_DIRS = [
 # Media folder settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  #added
 MEDIA_URL = '/media/'                         #added
+
+# Messages
+from django.contrib.messages import constants as messages  # added from django website
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger'
+}
+
+# Email Config
+EMAIL_HOST = 'smtp@gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'karan@gmail.com'
+EMAIL_HOST_PASSWORD = 'admin'
+EMAIL_USE_TLS = True 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
